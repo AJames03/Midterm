06d578436b55d0d043961a53e43253e12d7f057a
AJames03 <119918197+AJames03@users.noreply.github.com>
2022-12-06 14:37:43 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define p printf
+#define s scanf
+
+/* Betinol, Aron James L.
+   Qualified to Vote
+*/
+    int age;
+
+int main()
+{
+    p("Enter Your Age : ");
+    s("%i", &age);
+
+    p("\n");
+
+    if(age >= 18){
+        p("You are qualified to vote!!!");
+    }
+    else{
+        p("Too Young");
+    }
+    p("\n");
+    return 0;
+}
