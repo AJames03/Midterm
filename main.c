e3b2834d92d7bdb9898744f9a9952cd0b1f1891a
AJames03 <119918197+AJames03@users.noreply.github.com>
2022-12-06 14:38:37 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define p printf
+#define sc scanf
+
+// Betinol, Aron James L.
+// BSIT 1F
+// Integer Comparison
+int main()
+{
+    int a, b;
+    p("Value of A: ");
+    sc("%i", &a);
+    p("Value of B: ");
+    sc("%i", &b);
+
+    p("\n");
+
+    if(a>b){
+        p("The Value of A is %i", a);
+    }
+    if(a<b){
+        p("The Value of B is %i", b);
+    }
+    if(a==b){
+        p("The Value of A is %i and it is equal to the Value of B is %i", a,b);
+    }
+
+    p("\n");
+    return 0;
+}
