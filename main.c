2ed543b3a2bfdebf7c3114696f252a676c776126
AJames03 <119918197+AJames03@users.noreply.github.com>
2022-12-06 14:34:07 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define p printf
+#define sc scanf
+
+    float quiz, assign, proj, cs, me, grade;
+
+int main()
+{
+    p("Class Standing : ");
+    sc("%f",&cs);
+
+    p("Quiz : ");
+    sc("%f",&quiz);
+
+    p("Assignment : ");
+    sc("%f",&assign);
+
+    p("Project : ");
+    sc("%f",&proj);
+
+    p("Major Exam : ");
+    sc("%f",&me);
+
+    grade = (cs*.10)+(quiz*.15)+(assign*.15)+(proj*.20)+(me*.40);
+
+    p("Grade : %.2f \n", grade);
+
+    return 0;
+}
